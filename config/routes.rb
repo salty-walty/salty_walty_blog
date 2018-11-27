b208e64d83ea6e992986e48c1cc40f8f5d1319f6
salty-walty <waltonsj3@gmail.com>
2018-11-27 14:45:06 -0500

    added nav bar and some styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :comments
   resources :posts
-  get 'index' => 'practice#index'
+  root 'posts#index'
   post 'index' => 'practice#index'
   get 'about' => 'practice#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
